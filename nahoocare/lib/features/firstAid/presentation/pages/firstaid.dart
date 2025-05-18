8c81d5b6d7c5f71d5e6336b0f794936683f928dc
Ashenafidejene <ashenafidejene75@gmail.com>
2025-05-18 17:41:01 +0300

    feat: Add Account and First Aid pages with basic UI structure

---
+++ nahoocare/lib/features/firstAid/presentation/pages/firstaid.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class Firstaid extends StatelessWidget {
+  const Firstaid({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: const Center(
+        child: Text('First Aid Page', style: TextStyle(fontSize: 24)),
+      ),
+    );
+  }
+}
