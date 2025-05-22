e16bcb4c2e9343900bb444b1b745d43a34e93eef
Ashenafidejene <ashenafidejene75@gmail.com>
2025-05-22 10:40:54 +0300

    feat: Add First Aid page with basic UI structure

---
+++ nahoocare/lib/features/firstAid/presentation/pages/first-aid-pages.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class FirstAidPage extends StatelessWidget {
+  const FirstAidPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(child: Center(child: Text('First Aid Page')));
+  }
+}
